76008ac9cd2bd322ac287c78364ca33620ccf855
Gabriel <shalitmontagne.g@gmail.com>
2021-12-10 12:51:55 -0800

    commit tree 0fd9449409ca7a6aa7be041a3a3f3823fda1eb75

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,6 +5,13 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchChange, setSearchChange] = useState("")
+
+  function handleSearch(event) {
+    console.log(searchChange)
+    setSearchChange(event.target.value)
+    
+  }
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
@@ -19,7 +26,11 @@ function ShoppingList({ items }) {
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter 
+        change= {searchChange}
+        onSearchChange= {handleSearch} 
+        onCategoryChange={handleCategoryChange} 
+      />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
